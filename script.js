6abb6f92b8dd807ab85b8412eb320c44a097e7ef
yu <420801@m.mie-u.ac.jp>
2022-03-07 09:18:56 +0900

    Change HTML and JavaScript file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
-init();
-function init() {
+setup();
+function setup() {
     const url = new URL(document.location.href);
     if (url.hostname.toLowerCase() === 'yu-64b.github.io') {
         putUrls();
@@ -9,12 +9,12 @@ function init() {
 }
 function putUrls() {
     const githubUrlDiv = Array.from(document.getElementsByTagName('div')).find(element => element.id === 'github_pages_url');
-    const starUrlDiv = Array.from(document.getElementsByTagName('div')).find(element => element.id === 'star_server_free_url');
+    const infinityFreeUrlDiv = Array.from(document.getElementsByTagName('div')).find(element => element.id === 'infinityfree_url');
     if (githubUrlDiv) {
         githubUrlDiv.insertAdjacentHTML('afterbegin', '<a target="_blank" rel="noopener noreferrer" href="https://docs.github.com/en/pages">Powered by GitHub Pages</a>');
     }
-    if (starUrlDiv) {
-        starUrlDiv.insertAdjacentHTML('afterbegin', '<a target="_blank" rel="noopener noreferrer" href="https://www.star.ne.jp/free/">Powered by Netowl\'s StarServerFree</a>');
+    if (infinityFreeUrlDiv) {
+        infinityFreeUrlDiv.insertAdjacentHTML('afterbegin', '<a target="_blank" rel="noopener noreferrer" href="https://infinityfree.net/">Powered by InfinityFree</a>');
     }
 }
 function putCounter() {
@@ -22,10 +22,10 @@ function putCounter() {
     if (counterDiv) {
         if (localStorage && localStorage.getItem('username') && typeof localStorage.getItem('username') === 'string') {
             const usernameString = localStorage.getItem('username');
-            counterDiv.insertAdjacentHTML('afterbegin', '<img src="http://yu64b.starfree.jp/counter/?parameter=0&username=' + usernameString + '">');
+            counterDiv.insertAdjacentHTML('afterbegin', '<img src="https://yu-64b.infinityfreeapp.com/?parameter=0&username=' + usernameString + '">');
         }
         else {
-            counterDiv.insertAdjacentHTML('afterbegin', '<img src="http://yu64b.starfree.jp/counter/?paramerter=0">');
+            counterDiv.insertAdjacentHTML('afterbegin', '<img src="https://yu-64b.infinityfreeapp.com/?paramerter=0">');
         }
     }
 }
