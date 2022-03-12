0ff10ef374b3531d775c5e506aeecf0c4744e6df
yu <420801@m.mie-u.ac.jp>
2022-03-13 06:46:34 +0900

    Change HTML and JavaScript and CSS file of GPA

--- gpa/script.js
+++ gpa/script.js
@@ -20,139 +20,113 @@ function handleFile(event) {
         const htmlText = event.target.result;
         const parser = new DOMParser();
         const doc = parser.parseFromString(htmlText, 'text/html');
-        createObjectAndTable(doc);
+        createTable(doc);
     };
     reader.readAsText(file);
 }
-function createObjectAndTable(doc) {
-    // create object
+function createTable(doc) {
+    // delete table
+    if (!tableElement)
+        return;
+    const tableHeadElement = tableElement.tHead;
+    const talbeBodyElement = tableElement.tBodies[0];
+    if (!tableHeadElement || !talbeBodyElement)
+        return;
+    // delete head texts
+    headTexts.length = 0;
+    // delete table head
+    while (tableHeadElement.lastChild) {
+        tableElement.removeChild(tableHeadElement.lastChild);
+    }
+    // delete table body
+    while (talbeBodyElement.lastChild) {
+        tableElement.removeChild(talbeBodyElement.lastChild);
+    }
+    // read table
     if (!(doc instanceof Document))
         return;
     const gradeTableElements = Array.from(doc.getElementsByTagName('table')).filter(tableElement => tableElement.className === 'singleTableLine');
     const gradeTableElement = gradeTableElements.find(tableElement => { var _a, _b; return (_b = ((_a = tableElement.parentElement) === null || _a === void 0 ? void 0 : _a.id) === 'singleTableArea') !== null && _b !== void 0 ? _b : false; });
     if (!gradeTableElement)
         return;
-    const rowElements = Array.from(gradeTableElement.rows);
-    const firstRowElement = rowElements.shift();
-    if (!firstRowElement)
+    const gradeRowElements = Array.from(gradeTableElement.rows);
+    // read table head
+    const gradeFirstRowElement = gradeRowElements.shift();
+    if (!gradeFirstRowElement)
         return;
-    const firstCellElements = Array.from(firstRowElement.cells);
+    const gradeFirstCellElements = Array.from(gradeFirstRowElement.cells);
     const tmpHeadTexts = [];
-    for (const cellElement of firstCellElements) {
-        if (!cellElement.textContent || cellElement.textContent.trim() === '')
+    for (const gradeCellElement of gradeFirstCellElements) {
+        if (!gradeCellElement.textContent || gradeCellElement.textContent.trim() === '')
             return;
-        tmpHeadTexts.push(cellElement.textContent.trim());
+        tmpHeadTexts.push(gradeCellElement.textContent.trim());
     }
-    headTexts.length = 0;
-    for (const tmpHeadText of tmpHeadTexts)
+    // create head texts
+    for (const tmpHeadText of tmpHeadTexts) {
         headTexts.push(tmpHeadText);
-    // create table
-    if (!tableElement)
-        return;
-    while (tableElement.lastChild) {
-        tableElement.removeChild(tableElement.lastChild);
     }
-    const tableHeadElement = document.createElement('thead');
-    const tableRowElement = document.createElement('tr');
+    // create table head
+    const rowElement = document.createElement('tr');
     for (const headText of headTexts.concat(additionalHeadTexts)) {
-        const tableCellElement = document.createElement('th');
-        tableCellElement.textContent = headText;
-        tableRowElement.appendChild(tableCellElement);
+        const cellElement = document.createElement('th');
+        cellElement.textContent = headText;
+        rowElement.appendChild(cellElement);
     }
-    tableHeadElement.appendChild(tableRowElement);
+    tableHeadElement.appendChild(rowElement);
     tableElement.appendChild(tableHeadElement);
-    // create object
-    tableObject.length = 0;
-    // create table
-    const talbeBodyElement = document.createElement('tbody');
-    tableElement.appendChild(talbeBodyElement);
-    // create object
-    for (const rowElement of rowElements) {
-        const cellElements = Array.from(rowElement.cells);
-        if (cellElements.slice(1).every(cell => !cell.textContent || cell.textContent.trim() === ''))
+    // create table body
+    for (const gradeRowElement of gradeRowElements) {
+        const gradeCellElements = Array.from(gradeRowElement.cells);
+        if (gradeCellElements.slice(1).every(cellElement => !cellElement.textContent || cellElement.textContent.trim() === ''))
             continue;
-        const rowObject = {};
-        // create table
         const tableRowElement = document.createElement('tr');
-        // create object
-        for (const [i, cellElement] of cellElements.entries()) {
-            // if (!cellElement.textContent || cellElement.textContent.trim() === '') continue;
-            // const cellText = cellElement.textContent.trim();
-            // cteate table
-            const tableCellElement = document.createElement('td');
+        for (const [i, gradeCellElement] of gradeCellElements.entries()) {
+            const cellElement = document.createElement('td');
             const labelElement = document.createElement('label');
             const textInputElement = document.createElement('input');
             labelElement.classList.add('outline');
-            labelElement.appendChild(textInputElement);
-            tableCellElement.appendChild(labelElement);
-            tableRowElement.appendChild(tableCellElement);
+            if (i === 1 || i === 3 || i === 4) {
+                textInputElement.type = 'number';
+            }
+            else {
+                textInputElement.type = 'text';
+            }
             if (i === 1 || i === 2 || i === 3 || i === 4 || i === 5) {
                 textInputElement.classList.add('small');
             }
             else {
                 textInputElement.classList.add('large');
             }
-            // create object
-            if (i === 1 || i === 3 || i === 4) {
-                // create table
-                textInputElement.type = 'number';
-                // create object
-                if (!cellElement.textContent || cellElement.textContent.trim() === '')
-                    continue;
-                const cellText = cellElement.textContent.trim();
-                if (!headTexts[i] || parseFloat(cellText) === NaN)
-                    return;
-                rowObject[headTexts[i]] = parseFloat(cellText);
-                // create table
+            if (gradeCellElement.textContent && gradeCellElement.textContent.trim() !== '') {
+                const cellText = gradeCellElement.textContent.trim();
                 textInputElement.value = cellText;
-                // create object
             }
-            else {
-                // create table
-                textInputElement.type = 'text';
-                // create object
-                if (!cellElement.textContent || cellElement.textContent.trim() === '')
-                    continue;
-                const cellText = cellElement.textContent.trim();
-                if (!headTexts[i])
-                    return;
-                rowObject[headTexts[i]] = cellText;
-                // create table
-                textInputElement.value = cellText;
-                // create object
-            }
+            labelElement.appendChild(textInputElement);
+            cellElement.appendChild(labelElement);
+            tableRowElement.appendChild(cellElement);
         }
-        // create table
         for (let i = 0; i < additionalHeadTexts.length; i++) {
-            const tableCellElement = document.createElement('td');
+            const cellElement = document.createElement('td');
+            const labelElement = document.createElement('label');
+            const textInputElement = document.createElement('input');
             if (i === 0 || i === 1) {
-                const labelElement = document.createElement('label');
-                const textInputElement = document.createElement('input');
                 labelElement.classList.add('outline');
                 textInputElement.type = 'number';
                 textInputElement.classList.add('small');
-                labelElement.appendChild(textInputElement);
-                tableCellElement.appendChild(labelElement);
             }
             else if (i === 2) {
-                const labelElement = document.createElement('label');
-                const textInputElement = document.createElement('input');
                 textInputElement.type = 'checkbox';
                 textInputElement.checked = true;
-                labelElement.appendChild(textInputElement);
-                tableCellElement.appendChild(labelElement);
             }
             else {
-                const textInputElement = document.createElement('input');
                 textInputElement.type = 'button';
-                tableCellElement.appendChild(textInputElement);
+                textInputElement.value = '削除';
             }
-            tableRowElement.appendChild(tableCellElement);
+            labelElement.appendChild(textInputElement);
+            cellElement.appendChild(labelElement);
+            tableRowElement.appendChild(cellElement);
         }
-        // create object
-        tableObject.push(rowObject);
-        // create table
         talbeBodyElement.appendChild(tableRowElement);
-        // create object
     }
+    tableElement.appendChild(talbeBodyElement);
 }
