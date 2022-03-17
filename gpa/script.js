3e38a13d95411ce53632fba07b25177568e0f4f5
yu <420801@m.mie-u.ac.jp>
2022-03-17 09:57:55 +0900

    Change JavaScript file of GPA

--- gpa/script.js
+++ gpa/script.js
@@ -23,16 +23,18 @@ if (tableHeadElement instanceof HTMLTableSectionElement) {
     tableHeadElement.appendChild(createHeadRow(headTexts.concat(additionalHeadTexts)));
 }
 function handleFile(event) {
-    if (!(event instanceof Event) || !(event.currentTarget instanceof HTMLInputElement) || !event.currentTarget.files)
+    if (!(event instanceof Event) || !(event.currentTarget instanceof HTMLInputElement) || !event.currentTarget.files) {
         return;
+    }
     const file = event.currentTarget.files[0];
-    if ((file === null || file === void 0 ? void 0 : file.type) !== 'text/html')
+    if (!(file instanceof File) || file.type !== 'text/html') {
         return;
+    }
     const reader = new FileReader();
-    reader.onload = (event) => {
-        var _a;
-        if (typeof ((_a = event.target) === null || _a === void 0 ? void 0 : _a.result) !== 'string')
+    reader.onload = event => {
+        if (!(event.target instanceof FileReader) || typeof event.target.result !== 'string') {
             return;
+        }
         const htmlText = event.target.result;
         const parser = new DOMParser();
         const doc = parser.parseFromString(htmlText, 'text/html');
@@ -41,8 +43,9 @@ function handleFile(event) {
     reader.readAsText(file);
 }
 function createTable(doc) {
-    if (!(tableHeadElement instanceof HTMLTableSectionElement) || !(talbeBodyElement instanceof HTMLTableSectionElement))
+    if (!(tableHeadElement instanceof HTMLTableSectionElement) || !(talbeBodyElement instanceof HTMLTableSectionElement)) {
         return;
+    }
     headTexts.length = 0;
     while (tableHeadElement.lastChild) {
         tableHeadElement.removeChild(tableHeadElement.lastChild);
@@ -51,22 +54,25 @@ function createTable(doc) {
         talbeBodyElement.removeChild(talbeBodyElement.lastChild);
     }
     const gradeTableElements = Array.from(doc.getElementsByClassName('singleTableLine'));
-    const gradeTableElement = gradeTableElements.find(tableElement => { var _a; return ((_a = tableElement.parentElement) === null || _a === void 0 ? void 0 : _a.id) === 'singleTableArea'; });
-    if (!(gradeTableElement instanceof HTMLTableElement))
+    const gradeTableElement = gradeTableElements.find(tableElement => tableElement instanceof HTMLTableElement && tableElement.parentElement instanceof HTMLDivElement && tableElement.parentElement.id === 'singleTableArea');
+    if (!(gradeTableElement instanceof HTMLTableElement)) {
         return;
+    }
     for (const [i, rowElement] of Array.from(gradeTableElement.rows).entries()) {
         const texts = readRowTexts(rowElement);
         if (i === 0) {
-            if (texts.some(text => text === ''))
+            if (texts.some(text => text === '')) {
                 return;
+            }
             for (const text of texts) {
                 headTexts.push(text);
             }
             tableHeadElement.appendChild(createHeadRow(headTexts.concat(additionalHeadTexts)));
         }
         else {
-            if (texts.slice(1).every(text => text === ''))
+            if (texts.slice(1).every(text => text === '')) {
                 continue;
+            }
             talbeBodyElement.appendChild(createBodyRow(texts));
         }
     }
@@ -84,23 +90,27 @@ function readRowTexts(rowElement) {
     return texts;
 }
 function addRow() {
-    if (!(talbeBodyElement instanceof HTMLTableSectionElement))
+    if (!(talbeBodyElement instanceof HTMLTableSectionElement)) {
         return;
+    }
     const texts = [];
     texts.length = headTexts.length;
     texts.fill('');
     talbeBodyElement.appendChild(createBodyRow(texts));
 }
 function removeRow(event) {
-    if (!(event instanceof Event) || !(event.currentTarget instanceof HTMLInputElement))
+    if (!(event instanceof Event) || !(event.currentTarget instanceof HTMLInputElement)) {
         return;
+    }
     const removeButtonInputElement = event.currentTarget;
     const rowElement = removeButtonInputElement.closest('tr');
-    if (!rowElement)
+    if (!rowElement) {
         return;
+    }
     const talbeBodyElement = rowElement.parentElement;
-    if (!talbeBodyElement)
+    if (!talbeBodyElement) {
         return;
+    }
     talbeBodyElement.removeChild(rowElement);
 }
 function createHeadRow(texts) {
@@ -172,22 +182,65 @@ function createBodyRow(texts) {
     return rowElement;
 }
 function calculateGP() {
-    var _a;
-    if (!(talbeBodyElement instanceof HTMLTableSectionElement))
+    const errorCells = [];
+    const warningCells = [];
+    const scoreToEvaluationTexts = ['D', 'D', 'D', 'D', 'D', 'D', 'C', 'B', 'A', 'AA', 'AA'];
+    if (!(talbeBodyElement instanceof HTMLTableSectionElement)) {
         return;
+    }
     for (const rowElement of talbeBodyElement.rows) {
         const cellElements = rowElement.cells;
-        const selectCellElement = cellElements[headTexts.length + 2];
-        if (!(((_a = selectCellElement === null || selectCellElement === void 0 ? void 0 : selectCellElement.firstChild) === null || _a === void 0 ? void 0 : _a.firstChild) instanceof HTMLInputElement))
-            return;
-        const checkboxInputElement = selectCellElement.firstChild.firstChild;
-        if (checkboxInputElement.checked)
-            continue;
         const creditCellElement = cellElements[1];
         const evaluationCellElement = cellElements[2];
         const scoreCellElement = cellElements[3];
         const GPCellElement = cellElements[headTexts.length];
         const GPxCreditCellElement = cellElements[headTexts.length + 1];
+        const selectCellElement = cellElements[headTexts.length + 2];
+        if (!(creditCellElement instanceof HTMLTableCellElement) || !(creditCellElement.firstChild instanceof HTMLLabelElement) || !(creditCellElement.firstChild.firstChild instanceof HTMLInputElement)) {
+            return;
+        }
+        if (!(evaluationCellElement instanceof HTMLTableCellElement) || !(evaluationCellElement.firstChild instanceof HTMLLabelElement) || !(evaluationCellElement.firstChild.firstChild instanceof HTMLInputElement)) {
+            return;
+        }
+        if (!(scoreCellElement instanceof HTMLTableCellElement) || !(scoreCellElement.firstChild instanceof HTMLLabelElement) || !(scoreCellElement.firstChild.firstChild instanceof HTMLInputElement)) {
+            return;
+        }
+        if (!(GPCellElement instanceof HTMLTableCellElement) || !(GPCellElement.firstChild instanceof HTMLLabelElement) || !(GPCellElement.firstChild.firstChild instanceof HTMLInputElement)) {
+            return;
+        }
+        if (!(GPxCreditCellElement instanceof HTMLTableCellElement) || !(GPxCreditCellElement.firstChild instanceof HTMLLabelElement) || !(GPxCreditCellElement.firstChild.firstChild instanceof HTMLInputElement)) {
+            return;
+        }
+        if (!(selectCellElement instanceof HTMLTableCellElement) || !(selectCellElement.firstChild instanceof HTMLLabelElement) || !(selectCellElement.firstChild.firstChild instanceof HTMLInputElement)) {
+            return;
+        }
+        const creditTextInputElement = creditCellElement.firstChild.firstChild;
+        const evaluationTextInputElement = evaluationCellElement.firstChild.firstChild;
+        const scoreTextInputElement = scoreCellElement.firstChild.firstChild;
+        const GPTextInputElement = GPCellElement.firstChild.firstChild;
+        const GPxCreditTextIntpueElement = GPxCreditCellElement.firstChild.firstChild;
+        const checkboxInputElement = selectCellElement.firstChild.firstChild;
+        if (!checkboxInputElement.checked) {
+            continue;
+        }
+        const inputCreditNumber = creditTextInputElement.value.trim() !== '' ? Number(creditTextInputElement.value.trim()) : NaN;
+        const inputEvaluationText = evaluationTextInputElement.value.trim().replace(/[\uFF01-\uFF5E]/g, text => String.fromCharCode(text.charCodeAt(0) - 0xFEE0)).toUpperCase();
+        const inputScoreNumber = scoreTextInputElement.value.trim() !== '' ? Number(scoreTextInputElement.value.trim()) : NaN;
+        if (scoreToEvaluationTexts[inputScoreNumber] === inputEvaluationText) {
+        }
+        else if (scoreToEvaluationTexts.find(evaluationText => inputEvaluationText === evaluationText) && inputScoreNumber in scoreToEvaluationTexts) {
+            warningCells.push(evaluationCellElement, scoreCellElement);
+        }
+        else if (scoreToEvaluationTexts.find(evaluationText => inputEvaluationText === evaluationText)) {
+            warningCells.push(scoreCellElement);
+        }
+        else if (inputScoreNumber in scoreToEvaluationTexts) {
+            warningCells.push(evaluationCellElement);
+        }
+        else {
+            errorCells.push(evaluationCellElement, scoreCellElement);
+            continue;
+        }
     }
 }
 function calculateGPA() {
