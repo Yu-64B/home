9d1817badc55e0a35dff6bda57e0b5e93d21a265
yu <420801@m.mie-u.ac.jp>
2022-03-23 17:59:38 +0900

    Change HTML and JavaScript and CSS file of GPA

--- gpa/script.js
+++ gpa/script.js
@@ -11,8 +11,16 @@ function setupGPA() {
     const calcGPAButtonInputElement = document.getElementById('calc-gpa-button-input');
     const addButtonInputElement = document.getElementById('add-button-input');
     const filterDetailsElement = document.getElementById('filter-outer-details');
-    const selectAllButtonInputElement = document.getElementById('select-all-button-input');
-    const deselectAllButtonInputElement = document.getElementById('deselect-all-button-input');
+    const selectAllFiltersButtonInputElement = document.getElementById('select-all-filters-button-input');
+    const deselectAlFilterslButtonInputElement = document.getElementById('deselect-all-filters-button-input');
+    const selectAllRowsButtonInputElement = document.getElementById('select-all-rows-button-input');
+    const deselectAllRowsButtonInputElement = document.getElementById('deselect-all-rows-button-input');
+    const selectRowsButtonInputElement = document.getElementById('select-rows-button-input');
+    const deselectRowsButtonInputElement = document.getElementById('deselect-rows-button-input');
+    const showAllRowsButtonInputElement = document.getElementById('show-all-rows-button-input');
+    const hiddenAllRowsButtonInputElement = document.getElementById('hidden-all-rows-button-input');
+    const showRowsButtonInputElement = document.getElementById('show-rows-button-input');
+    const hiddenRowsButtonInputElement = document.getElementById('hidden-rows-button-input');
     if (tableHeadElement instanceof HTMLTableSectionElement) {
         tableHeadElement.appendChild(createHeadRow());
     }
@@ -24,7 +32,6 @@ function setupGPA() {
         readTableButtonInputElement.addEventListener('click', () => {
             createTableFromText();
             createFilter();
-            updateFilter();
         });
     }
     if (calcGPAButtonInputElement instanceof HTMLInputElement) {
@@ -34,13 +41,57 @@ function setupGPA() {
         addButtonInputElement.addEventListener('click', addRow);
     }
     if (filterDetailsElement instanceof HTMLDetailsElement) {
-        filterDetailsElement.addEventListener('toggle', updateFilter);
+        filterDetailsElement.addEventListener('toggle', updateFilters);
     }
-    if (selectAllButtonInputElement instanceof HTMLInputElement) {
-        selectAllButtonInputElement.addEventListener('click', selectAll);
+    if (selectAllFiltersButtonInputElement instanceof HTMLInputElement) {
+        selectAllFiltersButtonInputElement.addEventListener('click', event => {
+            selectAllFilters(event, true);
+        });
     }
-    if (deselectAllButtonInputElement instanceof HTMLInputElement) {
-        deselectAllButtonInputElement.addEventListener('click', deselectAll);
+    if (deselectAlFilterslButtonInputElement instanceof HTMLInputElement) {
+        deselectAlFilterslButtonInputElement.addEventListener('click', event => {
+            selectAllFilters(event, false);
+        });
+    }
+    if (selectAllRowsButtonInputElement instanceof HTMLInputElement) {
+        selectAllRowsButtonInputElement.addEventListener('click', _ => {
+            selectAllRows(true);
+        });
+    }
+    if (deselectAllRowsButtonInputElement instanceof HTMLInputElement) {
+        deselectAllRowsButtonInputElement.addEventListener('click', _ => {
+            selectAllRows(false);
+        });
+    }
+    if (selectRowsButtonInputElement instanceof HTMLInputElement) {
+        selectRowsButtonInputElement.addEventListener('click', _ => {
+            selectRows(true);
+        });
+    }
+    if (deselectRowsButtonInputElement instanceof HTMLInputElement) {
+        deselectRowsButtonInputElement.addEventListener('click', _ => {
+            selectRows(false);
+        });
+    }
+    if (showAllRowsButtonInputElement instanceof HTMLInputElement) {
+        showAllRowsButtonInputElement.addEventListener('click', _ => {
+            showAllRows(true);
+        });
+    }
+    if (hiddenAllRowsButtonInputElement instanceof HTMLInputElement) {
+        hiddenAllRowsButtonInputElement.addEventListener('click', _ => {
+            showAllRows(false);
+        });
+    }
+    if (showRowsButtonInputElement instanceof HTMLInputElement) {
+        showRowsButtonInputElement.addEventListener('click', _ => {
+            showRows(true);
+        });
+    }
+    if (hiddenRowsButtonInputElement instanceof HTMLInputElement) {
+        hiddenRowsButtonInputElement.addEventListener('click', _ => {
+            showRows(false);
+        });
     }
 }
 function handleFile(event) {
@@ -61,7 +112,6 @@ function handleFile(event) {
         const gradeDocument = parser.parseFromString(htmlText, 'text/html');
         createTable(gradeDocument);
         createFilter();
-        updateFilter();
     };
     reader.readAsText(file);
 }
@@ -103,12 +153,7 @@ function createTable(gradeDocument) {
 function readRowTexts(rowElement) {
     const texts = [];
     for (const cellElement of rowElement.cells) {
-        if (cellElement.innerText) {
-            texts.push(cellElement.innerText.trim());
-        }
-        else {
-            texts.push('');
-        }
+        texts.push(cellElement.innerText.trim());
     }
     return texts;
 }
@@ -145,7 +190,7 @@ function createBodyRow(texts) {
             textInputElement.classList.add('large');
         }
         textInputElement.value = text;
-        textInputElement.addEventListener('change', updateFilter);
+        textInputElement.addEventListener('change', updateFilters);
         labelElement.appendChild(textInputElement);
         cellElement.appendChild(labelElement);
         rowElement.appendChild(cellElement);
@@ -240,13 +285,13 @@ function calculateGPA() {
         }
         const inputEvaluationText = evaluationTextInputElement.value.replace(/[\uFF01-\uFF5E]/g, char => String.fromCharCode(char.charCodeAt(0) - 0xFEE0)).toUpperCase().trim();
         const inputScoreNumber = scoreTextInputElement.value.trim() !== '' ? Number(scoreTextInputElement.value.trim()) : NaN;
-        const checkedEvaluationText = scoreToEvaluationTexts.find(evaluationText => inputEvaluationText === evaluationText);
+        const filteredEvaluationText = scoreToEvaluationTexts.find(evaluationText => inputEvaluationText === evaluationText);
         if (scoreToEvaluationTexts[inputScoreNumber] !== inputEvaluationText) {
-            if (checkedEvaluationText && inputScoreNumber in scoreToEvaluationTexts) {
+            if (filteredEvaluationText && inputScoreNumber in scoreToEvaluationTexts) {
                 evaluationCellElement.classList.add('warning');
                 scoreCellElement.classList.add('warning');
             }
-            else if (checkedEvaluationText) {
+            else if (filteredEvaluationText) {
                 scoreCellElement.classList.add('warning');
             }
             else if (inputScoreNumber in scoreToEvaluationTexts) {
@@ -258,7 +303,7 @@ function calculateGPA() {
                 continue;
             }
         }
-        const GPNumber = Math.min(Math.max((checkedEvaluationText ? scoreToEvaluationTexts.lastIndexOf(checkedEvaluationText) : inputScoreNumber) - 5, 0), 4);
+        const GPNumber = Math.min(Math.max((filteredEvaluationText ? scoreToEvaluationTexts.lastIndexOf(filteredEvaluationText) : inputScoreNumber) - 5, 0), 4);
         GPTextInputElement.value = GPNumber.toString();
         const GPxCreditNumber = isNaN(inputCreditNumber) ? 0 : inputCreditNumber * GPNumber;
         GPxCreditTextIntputElement.value = GPxCreditNumber.toString();
@@ -310,10 +355,14 @@ function createFilter() {
         const filterTableElement = document.createElement('table');
         selectAllButtonInputElement.type = 'button';
         selectAllButtonInputElement.value = '全選択';
-        selectAllButtonInputElement.addEventListener('click', selectAll);
+        selectAllButtonInputElement.addEventListener('click', event => {
+            selectAllFilters(event, true);
+        });
         deselectAllButtonInputElement.type = 'button';
         deselectAllButtonInputElement.value = '全非選択';
-        deselectAllButtonInputElement.addEventListener('click', deselectAll);
+        deselectAllButtonInputElement.addEventListener('click', event => {
+            selectAllFilters(event, false);
+        });
         summaryElement.appendChild(textNode);
         detailsElement.appendChild(summaryElement);
         detailsElement.appendChild(selectAllButtonInputElement);
@@ -321,8 +370,9 @@ function createFilter() {
         detailsElement.appendChild(filterTableElement);
         filterOuterDivElement.appendChild(detailsElement);
     }
+    updateFilters();
 }
-function updateFilter() {
+function updateFilters() {
     if (!(talbeBodyElement instanceof HTMLTableSectionElement)) {
         return;
     }
@@ -334,12 +384,16 @@ function updateFilter() {
         return;
     }
     const filterTableElements = filterOuterDivElement.getElementsByTagName('table');
-    const newFilterMaps = [];
+    const previousFilterMaps = createFilterMaps();
+    for (const filterTableElement of filterTableElements) {
+        while (filterTableElement.lastChild) {
+            filterTableElement.removeChild(filterTableElement.lastChild);
+        }
+    }
     for (const [i, filterTableElement] of Array.from(filterTableElements).entries()) {
-        const previousFilterMap = new Map();
-        for (const filterRowElement of filterTableElement.rows) {
-            const filterCheckboxInputElement = filterRowElement.getElementsByTagName('input')[0];
-            previousFilterMap.set(filterRowElement.innerText.trim(), filterCheckboxInputElement.checked);
+        const previousFilterMap = previousFilterMaps[i];
+        if (!previousFilterMap) {
+            return;
         }
         const newFilterMap = new Map();
         for (const rowElement of talbeBodyElement.rows) {
@@ -347,49 +401,36 @@ function updateFilter() {
             if (!cellElement) {
                 return;
             }
-            const inputElement = cellElement.getElementsByTagName('input')[0];
-            if (!inputElement) {
-                return;
-            }
-            const inputText = inputElement.value.trim();
-            const previousCheckedBoolean = previousFilterMap.get(inputText);
-            if (previousCheckedBoolean === false) {
-                newFilterMap.set(inputText, previousCheckedBoolean);
+            const inputText = Array.from(cellElement.getElementsByTagName('input')).map(inputElement => inputElement.value.trim()).join('');
+            const previousFilterBoolean = previousFilterMap.get(inputText);
+            if (previousFilterBoolean === false) {
+                newFilterMap.set(inputText, false);
             }
             else {
                 newFilterMap.set(inputText, true);
             }
         }
-        newFilterMaps.push(newFilterMap);
-    }
-    for (const filterTableElement of filterTableElements) {
-        while (filterTableElement.lastChild) {
-            filterTableElement.removeChild(filterTableElement.lastChild);
-        }
-    }
-    for (const [i, filterTableElement] of Array.from(filterTableElements).entries()) {
-        const newFilterMap = newFilterMaps[i];
-        if (!newFilterMap) {
-            return;
-        }
-        for (const [filterText, checkedBoolean] of newFilterMap) {
-            const filterRowElement = document.createElement('tr');
-            const filterCellElement = document.createElement('td');
-            const filterLabelElement = document.createElement('label');
-            const filterCheckboxInputElement = document.createElement('input');
-            const filterTextNode = document.createTextNode(filterText);
-            filterCheckboxInputElement.type = 'checkbox';
-            filterCheckboxInputElement.checked = checkedBoolean;
-            filterLabelElement.appendChild(filterCheckboxInputElement);
-            filterLabelElement.appendChild(filterTextNode);
-            filterCellElement.appendChild(filterLabelElement);
-            filterRowElement.appendChild(filterCellElement);
-            filterTableElement.appendChild(filterRowElement);
+        for (const [filterText, filterBoolean] of newFilterMap) {
+            filterTableElement.appendChild(createFilterRow(filterText, filterBoolean));
         }
     }
 }
-function selectAll(event) {
-    if (!(event instanceof Event) || !(event.currentTarget instanceof HTMLInputElement)) {
+function createFilterRow(filterText, checkedBoolean) {
+    const filterRowElement = document.createElement('tr');
+    const filterCellElement = document.createElement('td');
+    const filterLabelElement = document.createElement('label');
+    const filterCheckboxInputElement = document.createElement('input');
+    const filterTextNode = document.createTextNode(filterText);
+    filterCheckboxInputElement.type = 'checkbox';
+    filterCheckboxInputElement.checked = checkedBoolean;
+    filterLabelElement.appendChild(filterCheckboxInputElement);
+    filterLabelElement.appendChild(filterTextNode);
+    filterCellElement.appendChild(filterLabelElement);
+    filterRowElement.appendChild(filterCellElement);
+    return filterRowElement;
+}
+function selectAllFilters(event, checkedBoolean) {
+    if (!(event.currentTarget instanceof HTMLInputElement)) {
         return;
     }
     const selectAllButtonInputElement = event.currentTarget;
@@ -398,21 +439,110 @@ function selectAll(event) {
         return;
     }
     for (const checkboxInputElement of detailsElement.getElementsByTagName('input')) {
-        checkboxInputElement.checked = true;
+        checkboxInputElement.checked = checkedBoolean;
     }
 }
-function deselectAll(event) {
-    if (!(event instanceof Event) || !(event.currentTarget instanceof HTMLInputElement)) {
+function selectRows(selectOrDeselectBoolean) {
+    if (!(talbeBodyElement instanceof HTMLTableSectionElement)) {
         return;
     }
-    const deselectAllButtonInputElement = event.currentTarget;
-    const detailsElement = deselectAllButtonInputElement.closest('details');
-    if (!detailsElement) {
+    const filterMaps = createFilterMaps();
+    selectAllRows(!selectOrDeselectBoolean);
+    for (const rowElement of talbeBodyElement.rows) {
+        const checkboxInputElement = Array.from(rowElement.getElementsByTagName('input')).find(inputElement => inputElement.type === 'checkbox');
+        const texts = readInputRowTexts(rowElement).slice(0, headTexts.length);
+        let checkedBoolean = selectOrDeselectBoolean;
+        if (!checkboxInputElement) {
+            return;
+        }
+        for (const [i, text] of texts.entries()) {
+            const filterMap = filterMaps[i];
+            if (!filterMap) {
+                return;
+            }
+            const filterBoolean = filterMap.get(text);
+            if (filterBoolean !== selectOrDeselectBoolean) {
+                checkedBoolean = !selectOrDeselectBoolean;
+            }
+        }
+        checkboxInputElement.checked = checkedBoolean;
+    }
+}
+function selectAllRows(checkedBoolean) {
+    if (!(talbeBodyElement instanceof HTMLTableSectionElement)) {
         return;
     }
-    for (const checkboxInputElement of detailsElement.getElementsByTagName('input')) {
-        checkboxInputElement.checked = false;
+    const checkboxInputElements = Array.from(talbeBodyElement.getElementsByTagName('input')).filter(inputElement => inputElement.type === 'checkbox');
+    for (const checkboxInputElement of checkboxInputElements) {
+        checkboxInputElement.checked = checkedBoolean;
     }
+}
+function showRows(showOrHiddenBoolean) {
+    if (!(talbeBodyElement instanceof HTMLTableSectionElement)) {
+        return;
+    }
+    const filterMaps = createFilterMaps();
+    showAllRows(!showOrHiddenBoolean);
+    for (const rowElement of talbeBodyElement.rows) {
+        const texts = readInputRowTexts(rowElement).slice(0, headTexts.length);
+        let checkedBoolean = showOrHiddenBoolean;
+        for (const [i, text] of texts.entries()) {
+            const filterMap = filterMaps[i];
+            if (!filterMap) {
+                return;
+            }
+            const filterBoolean = filterMap.get(text);
+            if (filterBoolean !== showOrHiddenBoolean) {
+                checkedBoolean = !showOrHiddenBoolean;
+            }
+        }
+        if (checkedBoolean) {
+            rowElement.classList.remove('hidden');
+        }
+        else {
+            rowElement.classList.add('hidden');
+        }
+    }
+}
+function showAllRows(checkedBoolean) {
+    if (!(talbeBodyElement instanceof HTMLTableSectionElement)) {
+        return;
+    }
+    for (const rowElement of talbeBodyElement.rows) {
+        if (checkedBoolean) {
+            rowElement.classList.remove('hidden');
+        }
+        else {
+            rowElement.classList.add('hidden');
+        }
+    }
+}
+function readInputRowTexts(rowElement) {
+    const texts = [];
+    for (const cellElement of rowElement.cells) {
+        const text = Array.from(cellElement.getElementsByTagName('input')).map(inputElement => inputElement.value.trim()).join('');
+        texts.push(text);
+    }
+    return texts;
+}
+function createFilterMaps() {
+    if (!(filterOuterDivElement instanceof HTMLDivElement)) {
+        return [];
+    }
+    const filterMaps = [];
+    const filterTableElements = filterOuterDivElement.getElementsByTagName('table');
+    for (const filterTableElement of filterTableElements) {
+        const filterMap = new Map();
+        for (const filterRowElement of filterTableElement.rows) {
+            const filterCheckboxInputElement = filterRowElement.getElementsByTagName('input')[0];
+            if (!filterCheckboxInputElement) {
+                return [];
+            }
+            filterMap.set(filterRowElement.textContent ? filterRowElement.textContent.trim() : '', filterCheckboxInputElement.checked);
+        }
+        filterMaps.push(filterMap);
+    }
+    return filterMaps;
 }
 function createTableFromText() {
     if (!(tableHeadElement instanceof HTMLTableSectionElement) || !(talbeBodyElement instanceof HTMLTableSectionElement)) {
@@ -452,8 +582,10 @@ function createTableFromText() {
             else {
                 const texts = [];
                 texts.length = headTexts.length;
-                texts[0] = cellTexts[0];
-                texts.fill('', 1);
+                texts.fill('');
+                if (cellTexts[0]) {
+                    texts[0] = cellTexts[0];
+                }
                 talbeBodyElement.appendChild(createBodyRow(texts));
             }
         }
